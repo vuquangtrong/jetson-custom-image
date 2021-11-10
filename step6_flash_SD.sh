dfcdbc8773aae3d1634441400c2c9c8d615e1d84
vuquangtrong <vuquangtrong@gmail.com>
2021-11-10 14:02:56 +0700

    fix flash to SD

--- step6_flash_SD.sh
+++ step6_flash_SD.sh
@@ -42,7 +42,7 @@ echo "Using ${IMAGE}"
 if [ "$(mount | grep $1)" ]; then
     echo "Unmount $1"
 	for mount_point in $(mount | grep $1 | awk '{ print $1}'); do
-		sudo umount $mount_point > /dev/null
+		sudo umount $mount_point 
 	done
 fi
 
@@ -53,8 +53,8 @@ dd if=${IMAGE} of=$1 bs=4M conv=fsync status=progress
 ##########
 echo "Extend the partition"
 
-partprobe $1 &> /dev/null
-sgdisk -e $1 > /dev/null
+partprobe $1
+sgdisk -e $1
 
 end_sector=$(sgdisk -p $1 |  grep -i "Total free space is" | awk '{ print $5 }')
 start_sector=$(sgdisk -i 1 $1 | grep "First sector" | awk '{print $3}')
@@ -62,15 +62,22 @@ start_sector=$(sgdisk -i 1 $1 | grep "First sector" | awk '{print $3}')
 echo "start_sector = ${start_sector}"
 echo "end_sector = ${end_sector}"
 
-sgdisk -d 1 $1 > /dev/null
-sgdisk -n 1:$start:$end $1 /dev/null
-sgdisk -c 1:APP $1 > /dev/null
+sgdisk -d 1 $1
+sgdisk -n 1:${start_sector}:${end_sector} $1
+sgdisk -c 1:APP $1
 
 ##########
 echo "Extend the filesystem"
 
-e2fsck -fp $1"p1" > /dev/null
-resize2fs $1"p1" > /dev/null
+if [[ $(basename $1) =~ "mmc*" ]]; then
+	e2fsck -fp $1"p1"
+	resize2fs $1"p1"
+fi
+
+if [[ $(basename $1) =~ "sd*" ]]; then
+	e2fsck -fp $1"1"
+	resize2fs $1"1"
+fi
 sync
 
 echo "DONE!"
